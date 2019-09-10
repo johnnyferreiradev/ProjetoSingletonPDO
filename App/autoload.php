00f48a44942386bc5455a26d94f9cce655bd2d8c
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-09-10 19:55:44 -0300

    Simplificação da estrutura

--- App/autoload.php
+++ App/autoload.php
@@ -1,8 +1,7 @@
 <?php
 
+    namespace App;
 
-namespace App;
-
-spl_autoload_register(function ($class){
-    require_once str_replace( '\\', '/', $class . ".php");
-});
+    spl_autoload_register(function ($class){
+        require_once str_replace( '\\', '/', $class . ".php");
+    });
