f4962a3cc6a6f81de2594163651b7514326796c5
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-09-12 11:31:00 -0300

    Criação do CRUD para a classe Ator

--- App/Classes/Ator.php
+++ App/Classes/Ator.php
@@ -10,6 +10,7 @@
         private $primeiroNome;
         private $ultimoNome;
         private $ultimaAtualizacao;
+        private $atorId;
 
         // Geters...
         public function getPrimeiroNome() {
@@ -24,6 +25,10 @@
             return $this->ultimaAtualizacao;
         }
 
+        public function getAtorId() {
+            return $this->atorId;
+        }
+
         // Seters...
         public function setPrimeiroNome($primeiroNome) {
             $this->primeiroNome = $primeiroNome;
@@ -35,6 +40,10 @@
 
         public function setUltimaAtualizacao($ultimaAtualizacao) {
             $this->ultimaAtualizacao = $ultimaAtualizacao;
+        }
+
+        public function setAtorId($atorId) {
+            $this->atorId = $atorId;
         }
 
         // Other methods...
@@ -61,16 +70,54 @@
         }
 
         public function update() {
-            // logica para atualizar cliente no banco
-        }
-        public function remove() {
-            // logica para remover cliente do banco
-        }
-        public function listAll() {
-            // logica para listar toodos os clientes do banco
-        }    
+            $pdo = Conexao::getInstance();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "UPDATE ator  set primeiro_nome = ?, ultimo_nome = ?, ultima_atualizacao = ? WHERE ator_id = ?";
+            $q = $pdo->prepare($sql);
+            $result = $q->execute(array($this->primeiroNome, $this->ultimoNome, $this->ultimaAtualizacao, $this->atorId));
+            Conexao::disconnect();
 
-        public function atorTeste() {
-            echo "Funcionou";
+            if ($result) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+        public function remove() {
+            $pdo = Conexao::getInstance();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "DELETE FROM ator where ator_id = ?";
+            $q = $pdo->prepare($sql);
+            $result = $q->execute(array($this->atorId));
+            Conexao::disconnect();
+
+            if ($result) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public function findById() {
+            $pdo = Conexao::getInstance();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "SELECT * FROM ator where ator_id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($this->atorId));
+            $data = $q->fetch(PDO::FETCH_ASSOC);
+            Conexao::disconnect();
+
+            return $data;
+        }  
+        
+        public function listAll() {
+            $pdo = Conexao::getInstance();
+            $sql = 'SELECT * FROM ator ORDER BY ator_id DESC';
+            $q = $pdo->query($sql);
+            $data = $q->fetchAll();
+            Conexao::disconnect();
+
+            return $data;
         }
     }
--- index.php
+++ index.php
@@ -6,19 +6,15 @@ use DB\Conexao as DB;
 use App\Classes\Ator;
 
 $ator1 = new Ator();
-$ator1->setPrimeiroNome('John');
-$ator1->setUltimoNome('Doe 1');
+$ator1->setPrimeiroNome('Será deletado');
+$ator1->setUltimoNome('1');
 $ator1->setUltimaAtualizacao(date('Y-m-d H:i:s'));
 
-echo $ator1->save();
+// Teste de inserção
+//echo $ator1->save();
 
-// $ator = Ator::atorTeste();
-// $banco = DB::getInstance();
+// Teste de atualização
+$ator1->setAtorId(201);
+$data = $ator1->listAll();
 
-// $consulta = $banco->query("SELECT * FROM cliente");
-
-// foreach($consulta as $linha) {
-//     echo "<pre>";
-//     print_r($linha);
-//     echo "</pre>";
-// }
+print_r($data);
