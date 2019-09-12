7b88cfda4d603d31cdce344c54f708e77f3bb493
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-09-12 16:39:51 -0300

    Criação da confirmação de exclusão e pagina de cadastro de ator

--- ListaAtor.php
+++ ListaAtor.php
@@ -3,7 +3,16 @@
 
     use App\Classes\Ator;
 
-    print_r(Ator::listAll());
+    $dados = Ator::listAll();
+
+    if(!empty($_GET)) {
+        if($_GET['modo'] == 'excluir') {
+            $atorId = $_GET['ator_id'];
+            $ator = new Ator();
+            $ator->setAtorId($atorId);
+            $resultado = $ator->remove();
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -16,5 +25,39 @@
 </head>
 <body>
     <h1>Lista de Atores cadastrados</h1>
+    <table style="width:100%">
+        <tr>
+            <th>ID</th>
+            <th>Primeiro Nome</th>
+            <th>Ultimo Nome</th>
+            <th>Ultima atualização</th>
+            <th>Ações</th>
+        </tr>
+        <?php foreach($dados as $ator) { ?>
+            <tr>
+                <td><?php echo $ator['ator_id']?></td>
+                <td><?php echo $ator['primeiro_nome']?></td>
+                <td><?php echo $ator['ultimo_nome']?></td>
+                <td><?php echo $ator['ultima_atualizacao']?></td>
+                <td>
+                    <form action="ListaAtor.php" method="POST">
+                        <input type="hidden" name="ator_id" value="<?php echo $ator['ator_id']?>">
+                        <input type="hidden" name="acao" value="editar">
+                        <button type="submit">Editar</button>
+                    </form>
+
+                    <button onclick="confirmarExclusao(<?php echo $ator['ator_id']; ?>)">Excluir</button>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+    <script language="Javascript">
+        function confirmarExclusao(id) {
+            let resposta = confirm('Tem certeza que deseja excluir?');
+            if (resposta) {
+                window.location.href = `ListaAtor.php?ator_id=${id}&modo=excluir`;
+            }
+        }
+    </script>
 </body>
 </html>
