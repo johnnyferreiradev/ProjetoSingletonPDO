4db98353491316e89ade7f0097a12c00a94e2309
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-09-13 15:49:47 -0300

    Testes na classe Pais

--- testes.php
+++ testes.php
@@ -4,17 +4,29 @@ require __DIR__.'/App/autoload.php';
 
 use DB\Conexao as DB;
 use App\Classes\Ator;
+use App\Classes\Pais;
 
-$ator1 = new Ator();
-$ator1->setPrimeiroNome('Será atualizado');
-$ator1->setUltimoNome('Atualizado com suc...');
-$ator1->setUltimaAtualizacao(date('Y-m-d H:i:s'));
-$ator1->setAtorId(209);
+// $ator1 = new Ator();
+// $ator1->setPrimeiroNome('Será atualizado');
+// $ator1->setUltimoNome('Atualizado com suc...');
+// $ator1->setUltimaAtualizacao(date('Y-m-d H:i:s'));
+// $ator1->setAtorId(209);
 
-// Teste de inserção
-echo $ator1->update();
+// // Teste de inserção
+// echo $ator1->update();
 
 // // Teste de atualização
 // $data = $ator1->listAll();
 
 // print_r($data);
+
+// Testes da classe Pais
+$pais = new Pais('Brazil1', date('Y-m-d H:i:s'), 110);
+//$pais->save(); // OK
+
+
+$pais->setPais('Brazil2');
+//$pais->setPaisId(110);
+$pais->update(); // Ok
+
+
