5983d21b050d8ce52a982198114d8cd8a5976d89
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-09-16 16:49:25 -0300

    Estilo tabela

--- App/Listas/ListaAluguel.php
+++ App/Listas/ListaAluguel.php
@@ -53,55 +53,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="../Assets/css/general.css">
+    <link rel="stylesheet" type="text/css" href="../Assets/css/table.css">
+    <script src="https://kit.fontawesome.com/2a2ac02fa4.js"></script>
     <title>Lista de alugueis</title>
-    <style>
-        table {
-            width: 100%;
-        }
-
-        table th {
-            border: 1px solid black;
-        }
-    </style>
 </head>
 <body>
     <h1 class="list-title">Lista de alugueis cadastrados</h1>
-    <a class="btn-new" href="../Formularios/FormAluguel.php">Novo registro</a>
-    <table class="list-table">
-        <tr class="row-titles">
-            <th>ID</th>
-            <th>Data de Aluguel</th>
-            <th>Inventario ID</th>
-            <th>Cliente ID</th>
-            <th>Data de devolução</th>
-            <th>Funcionario ID</th>
-            <th>Ultima atualização</th>
-            <th>Ações</th>
-        </tr>
-        <?php foreach($dadosPaginados as $aluguel) { ?>
-            <tr class="row-results">
-                <td><?php echo $aluguel['aluguel_id']?></td>
-                <td><?php echo $aluguel['data_de_aluguel']?></td>
-                <td><?php echo $aluguel['inventario_id']?></td>
-                <td><?php echo $aluguel['cliente_id']?></td>
-                <td><?php echo $aluguel['data_de_devolucao']?></td>
-                <td><?php echo $aluguel['funcionario_id']?></td>
-                <td><?php echo $aluguel['ultima_atualizacao']?></td>
-                <td>
-                    <form action="../Formularios/FormAluguel.php" method="POST">
-                        <input type="hidden" name="aluguel_id" value="<?php echo $aluguel['aluguel_id']?>">
-                        <input type="hidden" name="acao" value="carregar_info">
-                        <button type="submit" class="btn-edit">Editar</button>
-                    </form>
-
-                    <button onclick="confirmarExclusao(<?php echo $aluguel['aluguel_id']; ?>, <?php echo $pagina; ?>)" class="btn-remove">Excluir</button>
-                </td>
+    <div class="container-table">
+        <a class="btn-new" href="../Formularios/FormAluguel.php"><i class="fas fa-plus-circle"></i> Novo registro</a>
+        <table class="list-table">
+            <tr class="row-titles">
+                <th>ID</th>
+                <th>Data de Aluguel</th>
+                <th>Inventario ID</th>
+                <th>Cliente ID</th>
+                <th>Data de devolução</th>
+                <th>Funcionario ID</th>
+                <th>Ultima atualização</th>
+                <th>Ações</th>
             </tr>
-        <?php } ?>
-    </table>
+            <?php foreach($dadosPaginados as $aluguel) { ?>
+                <tr class="row-results">
+                    <td><?php echo $aluguel['aluguel_id']?></td>
+                    <td><?php echo $aluguel['data_de_aluguel']?></td>
+                    <td><?php echo $aluguel['inventario_id']?></td>
+                    <td><?php echo $aluguel['cliente_id']?></td>
+                    <td><?php echo $aluguel['data_de_devolucao']?></td>
+                    <td><?php echo $aluguel['funcionario_id']?></td>
+                    <td><?php echo $aluguel['ultima_atualizacao']?></td>
+                    <td>
+                        <form action="../Formularios/FormAluguel.php" method="POST">
+                            <input type="hidden" name="aluguel_id" value="<?php echo $aluguel['aluguel_id']?>">
+                            <input type="hidden" name="acao" value="carregar_info">
+                            <button type="submit" class="btn-edit">Editar</button>
+                        </form>
 
-    <a href="ListaAluguel.php?acao=&pagina=<?php echo $anterior; ?>" class="btn-previus">Anterior</a>
-    <a href="ListaAluguel.php?acao=&pagina=<?php echo $proxima; ?>" class="btn-next">Proxima</a>
+                        <button onclick="confirmarExclusao(<?php echo $aluguel['aluguel_id']; ?>, <?php echo $pagina; ?>)" class="btn-remove">Excluir</button>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+
+        <a href="ListaAluguel.php?acao=&pagina=<?php echo $anterior; ?>" class="btn-previus">Anterior</a>
+        <a href="ListaAluguel.php?acao=&pagina=<?php echo $proxima; ?>" class="btn-next">Proxima</a>
+    </div>
 
     <script language="Javascript">
         function confirmarExclusao(id, pagina) {
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./App/Assets/css/general.css">
     <link rel="stylesheet" type="text/css" href="./App/Assets/css/home.css">
     <script src="https://kit.fontawesome.com/2a2ac02fa4.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
