fef54d84e3e0ca12b25195be5539daabcda23f34
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-09-16 16:05:45 -0300

    Estilo da pagina home

--- index.php
+++ index.php
@@ -1,27 +1,76 @@
-<?php
-    
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./App/Assets/css/home.css">
+    <script src="https://kit.fontawesome.com/2a2ac02fa4.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <title>Banco de dados</title>
 </head>
-<body>
-    <h1>Sistema da Locadora!</h1>
-    <a href="App/Listas/ListaAtor.php?acao=&pagina=1">Atores</a><br>
-    <a href="App/Listas/ListaFilme.php?acao=&pagina=1">Filmes</a><br>
-    <a href="App/Listas/ListaIdioma.php?acao=&pagina=1">Idiomas</a><br>
-    <a href="App/Listas/ListaCategoria.php?acao=&pagina=1">Categorias</a><br>
-    <a href="App/Listas/ListaCidade.php?acao=&pagina=1">Cidades</a><br>
-    <a href="App/Listas/ListaAluguel.php?acao=&pagina=1">Alugueis</a><br>
-    <a href="App/Listas/ListaFuncionario.php?acao=&pagina=1">Funcionarios</a><br>
-    <a href="App/Listas/ListaCliente.php?acao=&pagina=1">Clientes</a><br>
-    <a href="App/Listas/ListaEndereco.php?acao=&pagina=1">Enderecos</a><br>
-    <a href="App/Listas/ListaPagamento.php?acao=&pagina=1">Pagamentos</a><br>
-    <a href="App/Listas/ListaPais.php">Países</a>
+<body id="menu-cadastro">
+    <div class="home">
+        <div class="container-home">
+            <h1 class="title-home">Registros da Locadora</h1>
+            <main class="links">
+                <a href="App/Listas/ListaAtor.php?acao=&pagina=1">
+                    <i class="fas fa-theater-masks"></i>
+                    <p>Atores</p>
+                </a>
+
+                <a href="App/Listas/ListaFilme.php?acao=&pagina=1">
+                    <i class="fas fa-film"></i>
+                    <p>Filmes</p>
+                </a>
+
+                <a href="App/Listas/ListaIdioma.php?acao=&pagina=1">
+                    <i class="fas fa-language"></i>
+                    <p>Idiomas</p>
+                </a>
+
+                <a href="App/Listas/ListaCategoria.php?acao=&pagina=1">
+                    <i class="fas fa-list"></i>
+                    <p>Categorias</p>
+                </a>
+
+                <a href="App/Listas/ListaCidade.php?acao=&pagina=1">
+                    <i class="fas fa-city"></i>
+                    <p>Cidades</p>
+                </a>
+
+                <a href="App/Listas/ListaAluguel.php?acao=&pagina=1">
+                    <i class="fas fa-file-alt"></i>
+                    <p>Alugueis</p>
+                </a>
+
+                <a href="App/Listas/ListaFuncionario.php?acao=&pagina=1">
+                    <i class="fas fa-briefcase"></i>
+                    <p>Funcionarios</p>
+                </a>
+
+                <a href="App/Listas/ListaCliente.php?acao=&pagina=1">
+                    <i class="fas fa-users"></i>
+                    <p>Clientes</p>
+                </a>
+
+                <a href="App/Listas/ListaEndereco.php?acao=&pagina=1">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <p>Enderecos</p>
+                </a>
+
+                <a href="App/Listas/ListaPagamento.php?acao=&pagina=1">
+                    <i class="fas fa-money-check-alt"></i>
+                    <p>Pagamentos</p>
+                </a>
+
+                <a href="App/Listas/ListaPais.php?acao=&pagina=1">
+                    <i class="fas fa-flag"></i>
+                    <p>Países</p>
+                </a>
+   
+            </main>
+        </div>
+    </div>
 </body>
 </html>
